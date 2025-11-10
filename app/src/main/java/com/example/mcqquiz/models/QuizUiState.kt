554408496bdb482ffde35297d84ceb7ba352c8c7
visheshtiwari <visheshtiwari211@gmail.com>
2025-11-10 17:20:59 +0530

    commit: added immutable to quiz ui state data class

--- app/src/main/java/com/example/mcqquiz/models/QuizUiState.kt
+++ app/src/main/java/com/example/mcqquiz/models/QuizUiState.kt
@@ -1,5 +1,8 @@
 package com.example.mcqquiz.models
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
 data class QuizUiState(
     val questions: List<McqUIModel> = emptyList(),
     val currentIndex: Int = 0,
